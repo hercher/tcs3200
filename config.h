47ac00cc22bc185cd2d3bf0a0ad0fffe250cdfc1
Florian Hercher <spam@florian-hercher.de>
2013-12-09 20:05:16 +0100

    cleanup

--- config.h
+++ config.h
@@ -1,11 +1,11 @@
 /*
  * define the pins the tcs3200 is connected to
  */
-#define TCS_S0_PIN				18
-#define TCS_S1_PIN				23
-#define TCS_S2_PIN				24
-#define TCS_S3_PIN				25
+#define TCS_S0_PIN          18
+#define TCS_S1_PIN          23
+#define TCS_S2_PIN          24
+#define TCS_S3_PIN          25
 
-#define TCS_ENABLE_PIN				8
-#define TCS_OUT_PIN				7
+#define TCS_ENABLE_PIN      8
+#define TCS_OUT_PIN         7
 
